6a4acb85a35b200414183df2eb61556ae5104f2e
smolivia <108894001+smolivia@users.noreply.github.com>
2022-08-08 14:32:06 -0700

    added test case and complexity analysis

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 The HeapPQEntry object, P priority object, and E data object were all written by the WashU Data Structures and Algorithms teaching team.
 
 In this project I implemented essential functions such as bubbleUp and bubbleDown to maintain the heap property of a min heap, allowing functions such as insert and remove to function correctly.
+In the test-cases-and-complexity text document, I also provide test cases, a complexity analysis, and data collected from running different input sizes to show that the complexity analysis is correct.
--- minHeap.java
+++ minHeap.java
@@ -1,30 +1,38 @@
-package min-heap;
+package heap;
 
 public class minHeap {
     //Heap storage: an array of Entry objects
     //Entry objects are essentially nodes in the heap
 	private final ArrayList<HeapPQEntry> entries;
+	//imported class to keep track of complexity, outputs of a graph showing how ticks grow as data set
+	//size grows
+	private final Ticker ticker;
 
     private void swap(int i, int j) {
 		//check that priorities are valid (greater than 0 and less than the total number of entries with 0 being the highest priority)
 		if(i >= 0 && i < entries.size() && j >= 0 && j < entries.size()) { 
+			ticker.tick(4); //if statement tests
 			//swap positions of entries
 			entries.get(i).position = j;
 			entries.get(j).position = i;
+			ticker.tick(2);
 			//swap actual entries
 			HeapPQEntry temp = entries.get(i);
 			entries.set(i, entries.get(j));
 			entries.set(j, temp);
+			ticker.tick(3);
 		}
 	}
 
 	@Override
 	public int size() {
+		ticker.tick();
 		return entries.size();
 	}
 
 	@Override
 	private void repairHeapAtEntry(MinHeap<E, P>.HeapPQEntry entry) {
+		ticker.tick(); //tick for assignment statement
 		int index = entry.position;
 		//execute bubbleUp and bubbleDown to make sure entries are correctly ordered
 		//according to their priorities
@@ -39,8 +47,10 @@ public class minHeap {
 		//set initial position to the current size of the entries and add to heap
 		e.position = entries.size();
 		entries.add(e);	
+		ticker.tick(3); //tick three times for three constant time statements above
 		//call repair method to bubble entry up or down according to its given priority	
 		repairHeapAtEntry(e);
+		ticker.tick(); //return statement
 		return e;
 	}
 
@@ -48,50 +58,63 @@ public class minHeap {
 	public PQEntry<E, P> extractMin() {
 		//return null if there are no entries in the min heap
 		if(entries.isEmpty()) {
+			ticker.tick(2); //if statement test and return statement
 			return null;
 		}
 		//swap current root with last element for easier removal from leaf position
 		swap(entries.size() - 1, 0);
 		//remove root (now swapped to the last element's position)
 		HeapPQEntry removed = entries.remove(entries.size() - 1);
+		ticker.tick(); //tick for getting the removed element
 		//update the new root to its correct position according to its priority
 		bubbleDown(0);
 		//return the removed root
+		ticker.tick(); //return statement
 		return removed;
 	}
 
 	//these methods are useful because the heap is stored in an ArrayList
 	private int parentIndex(int index) { //return index of parent of the entry at the given index
+		ticker.tick();//return statement
 		return (index-1)/2;
 	}
 	private int leftChildIndex(int index) { //return index of left child of entry at given index
+		ticker.tick();//return statement
 		return 2*index+1;
 	}
 	private int rightChildIndex(int index) { //return index of right child of entry at given index
+		ticker.tick();//return statement
 		return 2*index+2;
 	}
 
 	private void bubbleDown(int startIndex) {
 		//base case: no more children, node is leaf node, cannot swap down any more
 		if(leftChildIndex(startIndex) >= entries.size()) {
+			ticker.tick(2); //if statement test and return
 			return;
 		}
 
 		//get priorities of the current entry and the left child's entry
+		ticker.tick(2);
 		P curMin = entries.get(startIndex).getPriority();
 		P lMin = entries.get(leftChildIndex(startIndex)).getPriority();
+		
 		
 		//find min child's index (child with higher priority)
 		//default to assuming left child has a higher priority
 		int minIndex = leftChildIndex(startIndex);
+		ticker.tick();  //assignment statement tick
 		if(rightChildIndex(startIndex) < entries.size()) { //if right child exists and has a higher priority than left child, update minIndex
+			ticker.tick(); //if statement test
 			if(lMin.compareTo(entries.get(rightChildIndex(startIndex)).getPriority()) > 0) { 
 				minIndex = rightChildIndex(startIndex);
+				ticker.tick(2); // if statement test and assignment
 			} 
 		}	
 		
 		//base case: no more heap violation, current entry is already smaller (has higher priority) than minimum child
 		if(curMin.compareTo(entries.get(minIndex).getPriority()) < 0) {
+			ticker.tick(2) //if statement test and return
 			return;
 		}
 		//if we reach this point there is still a heap violation
@@ -104,13 +127,16 @@ public class minHeap {
 		//get priorities of current entry and the current entry's parent 
 		P parent = entries.get(parentIndex(startIndex)).getPriority();
 		P cur = entries.get(startIndex).getPriority();
+		ticker.tick(2); //get priorities ticks
 
 		//base case: reached root, cannot swap up any more
 		if(startIndex <= 0) {
+			ticker.tick(2) // if statement and return
 			return;
 		}
 		//swap if current entry's priority is less than (aka higher than) parent's priority
 		if(cur.compareTo(parent) < 0) {
+			ticker.tick() //if statement test
 			swap(startIndex, parentIndex(startIndex));
 			bubbleUp(parentIndex(startIndex)); //recursively travel to the index that the entry has been swapped to
 		}
